2290c5c2bb9bcba5a93ae3220047ca52e0df4acc
paparts <icpep.org@gmail.com>
2012-11-15 00:04:04 +0800

    Add init account confirmation on after registration

--- application/controllers/confirm.php
+++ application/controllers/confirm.php
@@ -5,8 +5,34 @@ class Confirm extends CI_Controller {
     }
     
     public function index(){
-        $content = file_get_contents(FCPATH.'templates/confirm.txt');
+        $token = $this->uri->segment(2);
         
-        echo $content;
+        if(!empty($token)) {
+            $sql = $this->db->query("
+                                        SELECT * from `pre_tokens` 
+                                        WHERE 
+                                        `token`='".$token."' AND `visited`='0' AND `date` > DATE_SUB(NOW(),INTERVAL 24 HOUR)
+                                    ");        
+            $num_res = $sql->num_rows();
+            
+            if($num_res === 1) {
+                
+                $array = array(
+                                "message"   =>  "Account successfully verified",
+                                "alert_type"=>  "success"
+                );
+                $this->session->set_userdata($array);
+                
+                redirect(base_url(), "location");
+                
+            } else {
+//            redirect to registration
+            }
+            
+            
+        } else {
+//            redirect to registration
+        }
+        
     }
 }
