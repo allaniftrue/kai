b81e3e2f288e607a32312a569b8ad919c6e0530f
paparts <icpep.org@gmail.com>
2012-11-15 00:01:25 +0800

    Update error messaging

--- application/controllers/home.php
+++ application/controllers/home.php
@@ -11,6 +11,9 @@ class Home extends CI_Controller {
 		$data['unique_name'] = uniqid(); 
 		$csrf_session = $this->session->userdata('hash_value');
 		$hash = ! empty($csrf_session) ? $csrf_session : random_string('alnum', 64);
+                
+                $message = $this->session->userdata("message");
+                $alert_type = $this->session->userdata("alert_type");
 		
 		if(empty($csrf_session)) {
 			$array = array(
@@ -19,7 +22,12 @@ class Home extends CI_Controller {
 			
 			$this->session->set_userdata($array);
 		}
-
+                
+                if(!empty($message) && ! empty($alert_type)) {
+                    $data["message"] = $message;
+                    $data["alert_type"] = $alert_type;
+                }
+                
 		$data['hash'] = $this->session->userdata('hash_value');
 		$this->load->view('home_view',$data);
 	}
