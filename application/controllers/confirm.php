6d0e3b2f0ebcc9a1f7541be6c839694c9f451a0b
paparts <icpep.org@gmail.com>
2012-11-16 00:41:49 +0800

    Update confirmation token and user status

--- application/controllers/confirm.php
+++ application/controllers/confirm.php
@@ -5,33 +5,74 @@ class Confirm extends CI_Controller {
     }
     
     public function index(){
+        
         $token = $this->uri->segment(2);
         
-        if(!empty($token)) {
+        if(! empty($token)) {
+            
             $sql = $this->db->query("
                                         SELECT * from `pre_tokens` 
                                         WHERE 
-                                        `token`='".$token."' AND `visited`='0' AND `date` > DATE_SUB(NOW(),INTERVAL 24 HOUR)
-                                    ");        
+                                        `token`=".$this->db->escape($token)." AND `visited`='0' AND `date` > DATE_SUB(NOW(),INTERVAL 24 HOUR)
+                                            AND `source`='registration'
+                                    ");   
+            
             $num_res = $sql->num_rows();
             
             if($num_res === 1) {
                 
+                $result = $sql->result();
+                
                 $array = array(
-                                "message"   =>  "Account successfully verified",
-                                "alert_type"=>  "success"
+                                "visited"=>'1'
                 );
-                $this->session->set_userdata($array);
+                
+                $this->db->update('pre_tokens', $array, array("uid"=>$result[0]->uid));
+                $aff_rows = $this->db->affected_rows();
+                
+                if($aff_rows === 1) {
+                    
+                    $data = array(
+                                    "confirmation"  =>  '1'
+                    );
+                    
+                    $this->db->update('pre_users', $data, array('id'=>$result[0]->id));
+                    $affected_rows = $this->db->affected_rows();
+                    
+                    if($affected_rows === 1) {
+                        $array = array(
+                                        "message"   =>  "Account successfully verified",
+                                        "alert_type"=>  "success"
+                        );
+
+                        $this->session->set_userdata($array);
+                    } else {
+                        $array = array(
+                                "message"   =>  "An error occurred while processing confirmation",
+                                "alert_type"=>  "error"
+                        );
+
+                        $this->session->set_userdata($array);
+                    }
+                    
+                } else {
+                    
+                    $array = array(
+                                "message"   =>  "An error occurred while processing confirmation",
+                                "alert_type"=>  "error"
+                    );
+                    $this->session->set_userdata($array);
+                }
                 
                 redirect(base_url(), "location");
                 
             } else {
-//            redirect to registration
+                redirect(base_url().'ref',"refresh");
             }
             
             
         } else {
-//            redirect to registration
+            redirect(base_url().'ref',"refresh");
         }
         
     }
