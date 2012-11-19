ac9ea886e75a7d2eba423c9b6e9733cc4941ccf7
paparts <icpep.org@gmail.com>
2012-11-20 00:31:49 +0800

    Update payments made calculation

--- application/controllers/dashboard.php
+++ application/controllers/dashboard.php
@@ -4,12 +4,15 @@ class Dashboard extends CI_Controller {
 	public function __construct() {
 		parent::__construct();
 		$this->load->model("Userq");
+                
 		$this->load->library("Mlib_headers");
 		$this->load->library("Mlib_trac");
+                
 		$this->mlib_trac->trac_login();
 	}
 
 	public function index() {
+                $data['payments_made'] = $this->Paymentsq->total_paid();
             
                 $data['userinfo'] = $this->Userq->user_profile($this->session->userdata('uid'));
 		$this->load->view('dashboard_view',$data);
